fa9f88eaa96bb33d93454e9602d29d276fb81edb
Patrik Mana <patrik.mana07@gmail.com>
2025-09-08 10:55:44 +0200

    .

---
+++ src/main/java/com/avmmealbuilder/springboot/AwmMealBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.avmmealbuilder.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmMealBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwmMealBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avmmealbuilder/springboot/AwmMealBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avmmealbuilder.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwmMealBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
