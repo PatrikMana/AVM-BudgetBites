64d9f5517519df0bda42a104c226fd5eb9592aa8
martinmacalik <119074659+martinmacalik@users.noreply.github.com>
2025-09-22 08:50:40 +0200

    Hello World

--- src/main/java/com/example/budgetbites/HomeController.java
+++ src/main/java/com/example/budgetbites/HomeController.java
@@ -1,11 +1,19 @@
 package com.example.budgetbites;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    @PostMapping("/clicked")
+    @ResponseBody
+    public String handleClick() {
+        return "You clicked the button!";
     }
 }
