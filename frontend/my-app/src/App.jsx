23518661405474ab889a9958a6e0781ed3c4aa43
Lukáš Večeřa <166996029+lukasvecera@users.noreply.github.com>
2025-09-29 09:32:40 +0200

    Working react bits

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -2,10 +2,16 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SplitText from "./src/components/SplitText.jsx";
+
 
 function App() {
   const [count, setCount] = useState(0)
   const [message, setMessage] = useState('')
+
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
 
   useEffect(() => {
     fetch('/api/hello')
@@ -38,6 +44,20 @@ function App() {
         Click on the Vite and React logos to learn more
           kokot
       </p>
+        <SplitText
+            text="Hello, GSAP!"
+            className="text-2xl font-semibold text-center"
+            delay={100}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+            onLetterAnimationComplete={handleAnimationComplete}
+        />
     </>
   )
 }
