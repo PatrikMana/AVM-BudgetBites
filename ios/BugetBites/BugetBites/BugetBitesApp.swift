04d6fab137110d1c381e4e335e7eaf1c9a8da941
Pluhec <jakplu@seznam.cz>
2025-11-10 10:39:24 +0100

    iOS initial

---
+++ ios/BugetBites/BugetBites/BugetBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugetBitesApp.swift
+//  BugetBites
+//
+//  Created by Jakub Pluháček on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BugetBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
